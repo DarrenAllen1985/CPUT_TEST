e8d330cef0bff69834c48865a1fe8069a4d90144
Darren <Darren.Allen1985@gmail.com>
2019-02-23 16:44:26 +0200

    Exercise 2

--- src/main/java/com/calculator/CalculatorInterfaceImplement.java
+++ src/main/java/com/calculator/CalculatorInterfaceImplement.java
@@ -3,6 +3,7 @@ package com.calculator;
 public class CalculatorInterfaceImplement implements CalculatorInterface {
   @Override
   public int add(int a, int b) {
+    return a+b;
     //return  Integer.parseInt(String.valueOf())
   }
 }
